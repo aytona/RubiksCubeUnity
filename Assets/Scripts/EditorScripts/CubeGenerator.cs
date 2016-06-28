38a19347f3e776478c5d940ae6e3db39bea6765a
aytona <christopher.aytona@gmail.com>
2016-06-28 02:15:19 -0400

    Cube dimensions fields added

--- Assets/Scripts/EditorScripts/CubeGenerator.cs
+++ Assets/Scripts/EditorScripts/CubeGenerator.cs
@@ -21,11 +21,11 @@ namespace EditorScripts
             {
                 cubeSize = new int[3];
             }
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.EndHorizontal();
+            GUILayout.Label("Cube Dimensions", EditorStyles.boldLabel);
             EditorGUILayout.BeginHorizontal();
             for (int i = 0; i < cubeSize.Length; i++)
             {
+                GUILayout.TextArea(char.ConvertFromUtf32(i + 88) + ":", EditorStyles.miniLabel); // Might be a better way to get the char of the axis
                 EditorGUILayout.IntField(cubeSize[i], GUILayout.ExpandWidth(false));
             }
             EditorGUILayout.EndHorizontal();
