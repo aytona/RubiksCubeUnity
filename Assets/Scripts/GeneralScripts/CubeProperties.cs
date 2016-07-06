141ee89886cf8a7583b2fd89cd378daf446391bc
aytona <christopher.aytona@gmail.com>
2016-07-05 22:03:53 -0400

    Cube properties class

---
+++ Assets/Scripts/GeneralScripts/CubeProperties.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Aytona
+{
+    public class CubeProperties
+    {
+        #region Singleton
+        private static CubeProperties instance;
+        private CubeProperties() { }
+        public static CubeProperties Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new CubeProperties();
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+    }
+}
